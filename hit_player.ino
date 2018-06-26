857405f3d39b77a0715d16559d47e44fe546647f
toshi <toshi.n.0520@gmail.com>
2018-06-26 18:07:24 +0900

    Add files via upload

---
+++ hit_player.ino
@@ -0,0 +1,34 @@
+void PlayerHit(x_size, y_size, )
+  if (y == (player_y + 10) ) {  // キャラに当たったとき
+    if (((x + 10) > player_x) && (x < (player_x + 10))){
+    PIXEL.freeSprite(0);
+    x = 10;
+    y = 27;
+    PIXEL.drawSprite(0, x, y);
+    }
+  }
+  if ((y + 10) == player_y) {  // キャラに当たったとき
+    if (((x + 10) > player_x) && (x < (player_x + 10))){
+    PIXEL.freeSprite(0);
+    x = 10;
+    y = 27;
+    PIXEL.drawSprite(0, x, y);
+    }
+  }
+  if (x == (player_x + 10) ) {  // キャラに当たったとき
+    if (((y + 10) > player_y) && (y < (player_y + 10))){
+    PIXEL.freeSprite(0);
+    x = 10;
+    y = 27;
+    PIXEL.drawSprite(0, x, y);
+    }
+  }
+  if ((x + 10) == player_x) {  // キャラに当たったとき
+    if (((y + 10) > player_y) && (y < (player_y + 10))){
+    PIXEL.freeSprite(0);
+    x = 10;
+    y = 27;
+    PIXEL.drawSprite(0, x, y);
+    }
+  }
+  
